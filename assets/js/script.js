61f0c6c421b776efde0d42166db40ab5760103ec
Snoopawoo <reki.csaba@gmail.com>
2023-01-02 15:51:15 +0000

    adds basic functions to get input and data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,30 @@
+var itemWrapper = $('.aaa');
+var searchInput = $('.weather-search');
+var city;
 var apiKey = '3899e7379039119a99a2e3d150c2d807';
-var city = 'London';
 var iconUrl = 'https://openweathermap.org/img/w/';
+//get user input
+function getInput() {
+  city = searchInput.val();
+  dataRequest();
+};
 
+function dataRequest() {
 $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-  .then(function(currentData) {
+.then(function(currentData) {
   console.log(`
-    Temp: ${Math.round(currentData.main.temp)}°C
-    Wind: ${currentData.wind.speed}mph
-    Humidity: ${currentData.main.humidity}%
-    Icon URL: ${iconUrl + currentData.weather[0].icon + '.png'}
+  Temp: ${Math.round(currentData.main.temp)}°C
+  Wind: ${currentData.wind.speed}mph
+  Humidity: ${currentData.main.humidity}%
+  Icon URL: ${iconUrl + currentData.weather[0].icon + '.png'}
   `);
   
   $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&appid=${apiKey}`)
-    .then(function(forecastData) {
+  .then(function(forecastData) {
     console.log(forecastData);
   })
 })
+};
+
+$( "#search-button" ).click(getInput);
+
