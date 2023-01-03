ea5b19570fe79611a7633937b373d94980a55ecd
Snoopawoo <reki.csaba@gmail.com>
2023-01-03 02:09:53 +0000

    adds code to store history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,41 @@
 var currentWrapper = $('#today');
 var forecastWrapper = $('#forecast');
+var historyWrapper = $('#history');
 var searchInput = $('.weather-search');
 var city;
 var apiKey = '3899e7379039119a99a2e3d150c2d807';
 var iconUrl = 'https://openweathermap.org/img/w/';
+var searchHistory = [];
+searchHistory = JSON.parse(localStorage.getItem('history'));
+
+//makes sure that we don't get an error when we first run the site and get a null from loading local storage
+if(searchHistory == null){
+  searchHistory = [];
+};
+
+//adds a button for each search history
+function appendHistory() {
+  historyWrapper.html('');
+  for (var history of searchHistory) {
+  historyWrapper.append(`
+  <p>${history}</p>
+  `)};
+};
+
+appendHistory();
+
 //get user input
 function getInput() {
   city = searchInput.val();
   dataRequest();
+  savetoHistory();
 };
 
+//prints data based on user input
 function dataRequest() {
 $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
 .then(function(currentData) {
   console.log(currentData);
-  // console.log(`
-  // Temp: ${Math.round(currentData.main.temp)}°C
-  // Wind: ${currentData.wind.speed}mph
-  // Humidity: ${currentData.main.humidity}%
-  // Icon URL: ${iconUrl + currentData.weather[0].icon + '.png'}
-  // `);
   
   $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&appid=${apiKey}&units=metric`)
   .then(function(forecastData) {
@@ -43,17 +59,15 @@ $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
   <p>Wind: ${currentData.wind.speed}mph</p>
   <p>Humidity: ${currentData.main.humidity}%</p>
   `);
-  //Icon URL: ${iconUrl + currentData.weather[0].icon + '.png'}
-  
-// forecastWrapper.html(`
-//   Temp: ${Math.round(currentData.main.temp)}°C
-//   Wind: ${currentData.wind.speed}mph
-//   Humidity: ${currentData.main.humidity}%
-//   Icon URL: ${iconUrl + currentData.weather[0].icon + '.png'}
-// `);
 })
+};
 
+//adds searches to history
+function savetoHistory() {
+    searchHistory.push(city);
+    console.log(searchHistory)
+    localStorage.setItem('history', JSON.stringify(searchHistory));
+    appendHistory();
 };
 
 $( "#search-button" ).click(getInput);
-
